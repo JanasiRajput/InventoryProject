ff209a2f9a9e008d39d43055441c2cd5bbaabfb6
JanasiRajput <rajputjanasi@gmail.com>
2026-02-19 17:44:13 -0500

    "Implemented stock checking by Janasi Rajput - 991725719"

--- src/main/java/com/mycompany/inventoryapp/InventoryApp.java
+++ src/main/java/com/mycompany/inventoryapp/InventoryApp.java
@@ -13,4 +13,16 @@ public class InventoryApp {
     public static void main(String[] args) {
         System.out.println("Hello World!");
     }
+    public int checkStockLevel(String item) {
+        // Dummy implementation
+        System.out.println("Checking stock level for " + item);
+        return 0;
+    }
+
+    // Method 2
+    public void alertLowStock(String item, int stock) {
+        if (stock < 5) {
+            System.out.println("ALERT: Low stock for " + item);
+}
+    }
 }
